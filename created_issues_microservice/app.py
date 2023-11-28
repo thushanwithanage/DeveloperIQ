3a8e0887c2ccf82151dfbcbf672109f31b3a14cc
thushanwithanage <68364096+thushanwithanage@users.noreply.github.com>
2023-11-28 18:12:25 +0000

    Added monthly stats route

--- created_issues_microservice/app.py
+++ created_issues_microservice/app.py
@@ -3,6 +3,7 @@ import uvicorn
 import json
 
 from services.get_user_records import get_users
+from services.get_stat_records import get_monthly_stats
 from services.save_record import save_dev_stats
 from services.read_github import get_github_data
 from services.filter_prs import filter_out_prs
@@ -12,7 +13,7 @@ from services.format_response import format_user_response, format_user_stats_res
 
 from utils.config import headers
 from utils.constants import created_issues_url
-from utils.format_stats import format_developer_stats
+from utils.format_stats import format_developer_stats, format_monthly_stats, format_final_stats
 
 from models.UserStats import UserStats
 
@@ -50,6 +51,17 @@ async def save_user_stats():
         save_stats_list.append(await save_stat(developer_stats))
     insert_status_data = format_user_stats_response(user_id_list, save_stats_list)
     return insert_status_data
+
+@app.get("/monthly_stats")
+async def get_dev_monthly_stats():
+    users = await read_users()
+    user_id_list, username_list = format_user_response(users)
+    userStats = []
+    for uid, uname in zip(user_id_list, username_list):
+        monthly_entries = json.loads(get_monthly_stats(str(uid)))
+        total_count = format_monthly_stats(monthly_entries)
+        userStats.append(format_final_stats(uid, uname, total_count))
+    return userStats
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8081, host="0.0.0.0")
--- created_issues_microservice/utils/format_stats.py
+++ created_issues_microservice/utils/format_stats.py
@@ -1,12 +1,28 @@
 from utils.date_utils import last_date
 from utils.constants import stat_type
+import json
 
 def format_developer_stats(userName,userId,created_issues_count):
     developer_stats = {
             "username": userName,
             "user_id": userId,
-            "stat_type": "created_issues",
+            "stat_type": stat_type,
             "count": created_issues_count,
             "date_value": last_date
         }
     return developer_stats
+
+def format_monthly_stats(response):
+    count = 0
+    for entry in response:
+        stat = json.loads(entry)
+        count = count + int(stat["count"])
+    return json.loads(str(count))
+
+def format_final_stats(userId,userName,count):
+    stats = {
+            "user_id": userId,
+            "username": userName,
+            "count": count
+        }
+    return stats
